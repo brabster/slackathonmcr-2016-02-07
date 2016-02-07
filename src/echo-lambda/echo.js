bd39f972c9365dbc2115f5237e9b2fd65b9fcfb0
Paul Brabban <paul@wakelet.com>
2016-02-07 12:30:36 +0000

    add some slacky response templating

--- src/echo-lambda/echo.js
+++ src/echo-lambda/echo.js
@@ -1,11 +1,39 @@
 'use strict';
 
-import http from 'http';
+var http = require('http');
+
+var buildResponse = function(url, statusCode) {
+  return {
+    "title": "HTTP code",
+    "text": "HTTP for: <" + url + "|" + url + ">",
+    "attachments": [
+      {
+        "color": "good",
+        "fields": [
+          {
+            "title": "HTTP status code",
+            "value": statusCode + "",
+            "short": false
+          }
+        ]
+      },
+      {
+        "color": "danger",
+        "fields": [
+          {
+            "title": "TTFB",
+            "value": "200",
+            "short": false
+          }
+        ]
+      }
+    ]
+  }
+
+};
 
 exports.handler = function(event, ctx) {
-  return http.get(event.text, function(err, res) {
-    if (err) return ctx.fail('Boom! Didn\'t work, sorry');
-    console.log(res);
-    return ctx.succeed(res.statusCode);
+  return http.get(event.text, function(res) {
+    return ctx.succeed(buildResponse(event.text, res.statusCode));
   });
 };
